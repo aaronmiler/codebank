95791afdbd0b0b45eb755de64500e8e778c44146
Aaron Miler <aaron@aaronmiler.com>
2013-10-01 11:18:43 -0700

    Fixing deployment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Knowledge::Application.config.secret_key_base = '2146e23bb686b750f22acd005be869b82c614e6e33f7bcaa7fabf1fbbc83e5af89faead9f6099e08e03b7f774a7a6f9fe20ae2d9d651ea61499d8dd30720f8bb'
